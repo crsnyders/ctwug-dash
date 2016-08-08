92f63ba091b7037513b376af91fb5a73f72e44be
Christopher Snyders <csnyders@thefieldoffice.co.za>
2016-08-08 13:49:29 +0200

    updated the search, shouldnt break anymore

--- server/search.js
+++ server/search.js
@@ -1,5 +1,5 @@
 var config = require('./config.js')
-var htmlToJson  = require('html-to-json');
+var cheerio = require('cheerio');
 var _ = require("lodash")
 var request = require('request');
 var Promise = require('promise');
@@ -7,148 +7,120 @@ var findPage = "page=";
 var findName = "name=";
 var urlBase = "http://torrents.ctwug.za.net/torrents-search.php?search=";
 //var urlBase = "http://127.0.0.1:8080/torrents/torrents-search.php?search=";
-Array.prototype.unique = function()
-{
-        var n = {},r=[];
-        for(var i = 0; i < this.length; i++)
-        {
-                if (!n[this[i]])
-                {
-                        n[this[i]] = true;
-                        r.push(this[i]);
-                }
-        }
-        return r;
-}
-var tableFilterOptions = {
-  'torrents': ['.t-row', function($item) {
-    var trow = $item.find('td').map(function(x, y) {
-      if (x == 0) {
-        return y.children[0].children[0].attribs['alt']
-      } else if (x == 1) {
-        return y.children[2].attribs['title']
-      } else if (x == 2) {
-        return y.children[0].attribs['href']
-      } else if (x == 3) {
-        return y.children[0].children[0].data
-      } else if (x == 5) {
-        return y.children[0].data
-      } else if (x == 6) {
-        return y.children[0].children[0].children[0].data
-      } else if (x == 7) {
-        return y.children[0].children[0].children[0].data
-      } else if (x == 8) {
-        return y.children[0].children[0].children[0].data
-      } else if (x == 12) {
-        return y.children[0].data
-      }
-
-    })
-    delete trow.options;
-    delete trow._root;
-    delete trow.prevObject;
-    delete trow.length;
-    return trow
-  }],
-  'pages':['p',function($items){
-    var links = []
-    for(index in $items[0].children){
-      var item = $items[0].children[index];
-      if(item.name ='a' && item.attribs != undefined && item.attribs.href != undefined){
-        var href = item.attribs.href;
-        var indexOfPage = href.indexOf(findPage)
-        links.push(href.substring(indexOfPage+ findPage.length));
-      }
+Array.prototype.unique = function() {
+  var n = {},
+    r = [];
+  for (var i = 0; i < this.length; i++) {
+    if (!n[this[i]]) {
+      n[this[i]] = true;
+      r.push(this[i]);
     }
-    return links.unique();
-  }]
-};
-
-function tableResultClean (result) {
-  for (var i = 0; i < result.torrents.length; i++) {
-    var torrent = result.torrents[i];
-
-    torrent.type = torrent['0'];
-    torrent.title = torrent['1'];
-    torrent.link = 'http://torrents.ctwug.za.net/'+torrent['2'];
-    torrent.uploader = torrent['3'];
-    torrent.size = torrent['4'];
-    torrent.seeds = torrent['5'];
-    torrent.leech = torrent['6'];
-    torrent.completed = torrent['7'];
-    torrent.added = torrent['8'];
-    torrent.name = decodeURIComponent(torrent.link.substring(torrent.link.indexOf(findName)+findName.length))
-    torrent.content= "Category: "+torrent.type+" Size: "+torrent.size+" Added: "+torrent.added+" Seeders: "+torrent.seeds+" Leechers: "+torrent.leech+" Completed: "+torrent.completed;
-
-    delete torrent['0'];
-    delete torrent['1'];
-    delete torrent['2'];
-    delete torrent['3'];
-    delete torrent['4'];
-    delete torrent['5'];
-    delete torrent['6'];
-    delete torrent['7'];
-    delete torrent['8'];
   }
-  var pages=[]
-  var currentPages = result.pages[0];
-  if(currentPages.length >0){
-    for (var i = parseInt(currentPages[0]); i < parseInt(currentPages[currentPages.length-1]); i++) {
-      pages.push(i)
-    }
-  }else{
-    pages = [0];
-  }
-  result.pages = pages;
-  return result;
+  return r;
 }
-function search(searchString, jar, page, body) {
+
+function search(searchString, jar, page) {
   page = _.isNumber(page) ? page : 0;
   var options = {
     uri: urlBase + encodeURIComponent(searchString) + "&page=" + page + "&cat=0&incldead=0&freeleech=0&lang=0",
     jar: jar,
     method: 'GET'
   };
-  if (body) {
-    retunr
-    return runSearchHtml(options);
-  } else {
-    return runSearch(options);
-  }
-}
-function searchUrl(searchURL,jar){
-  var options = {
-          uri : searchURL,
-          jar : jar,
-          method : 'GET'
-        };
   return runSearch(options);
 }
 
-function runSearchHtml(options){
-  var promise  = new Promise(function(resolve,reject){
-    request(options,function (error, response, body){
-            var prom = runSearchOnString(body)
-            prom.then(function(result){
-              resolve(result);
-            })
-            .catch(function(error){
-              reject(error);
-            });
-          });
+function searchUrl(searchURL, jar) {
+  var options = {
+    uri: searchURL,
+    jar: jar,
+    method: 'GET'
+  };
+  return runSearch(options);
+}
+
+function runSearch(options) {
+  var promise = new Promise(function(resolve, reject) {
+    request(options, function(error, response, body) {
+      if (!error) {
+        var $ = cheerio.load(body);
+        var torrents = extractTorrentInfo($);
+        var pages = extractPages()
+        resolve({
+          "torrents": torrents,
+          "pages": pages
+        });
+      } else {
+        reject(error);
+      }
+    });
   });
   return promise;
 }
 
-function runSearchOnString(body){
-  var promise = htmlToJson.parse(body, tableFilterOptions);
-  promise.then(tableResultClean);
-  return promise
-}
-function runSearch(options){
-  var promise = htmlToJson.request(options, tableFilterOptions);
-  promise.then(tableResultClean);
-  return promise
+function extractPages($) {
+  var findPage = "page=";
+  var links = $('a');
+  pages = []
+  for (var i = 0; i < links.length; i++) {
+    var link = $(links[i])
+    if (link.attr('href').indexOf('page') != -1) {
+      var href = link.attr('href');
+      var indexOfPage = href.indexOf(findPage)
+      pages.push(href.substring(indexOfPage + findPage.length));
+    }
+  }
+  return pages.unique();
 }
 
-module.exports = {search: search,searchUrl:searchUrl};
+
+function extractTorrentInfo($) {
+  var rows = $(".t-row");
+  var torrents = [];
+  for (var i = 0; i < rows.length; i++) {
+    var row = $(rows[i]);
+    var torrent = {};
+    for (var j = 0; j < row.children().length; j++) {
+      var child = $(row.children()[j]);
+      var data = _.trim(child.children().text())
+      if (_.isEmpty(data)) {}
+      switch (j) {
+        case 0:
+          torrent.type = $("[alt]", child).first().attr('alt');
+          break;
+        case 1:
+          torrent.title = $("[title]", child).first().attr('title');
+          break;
+        case 2:
+          torrent.link = $('a', child).first().attr('href');
+          break;
+        case 3:
+          torrent.uploader = child.text()
+          break;
+        case 5:
+          torrent.size = child.text()
+          break;
+        case 6:
+          torrent.seeds = child.text()
+          break;
+        case 7:
+          torrent.leech = child.text()
+          break;
+        case 8:
+          torrent.completed = child.text()
+          break;
+        case 12:
+          torrent.added = child.text()
+          break;
+      }
+    }
+    torrent.name = decodeURIComponent(torrent.link.substring(torrent.link.indexOf(findName) + findName.length))
+    torrent.content = "Category: " + torrent.type + " Size: " + torrent.size + " Added: " + torrent.added + " Seeders: " + torrent.seeds + " Leechers: " + torrent.leech + " Completed: " + torrent.completed;
+    torrents.push(torrent);
+  }
+  return torrents;
+}
+
+module.exports = {
+  search: search,
+  searchUrl: searchUrl
+};
