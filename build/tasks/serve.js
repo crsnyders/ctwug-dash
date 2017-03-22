418295f121ebdfcbb2a86024e3f578537f81faf4
Christopher Snyders <csnyders@thefieldoffice.co.za>
2017-03-22 14:56:41 +0200

    starting new changes

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var url = require('url');
+var proxy = require('proxy-middleware');
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
+
+
+var apiProxyOptions = url.parse('https://estellesnyders.ip.afrihost.co.za/rest');
+apiProxyOptions.route = '/rest';
+apiProxyOptions.rejectUnauthorized= false,
 gulp.task('serve', ['build'], function(done) {
   browserSync({
     online: false,
@@ -11,10 +18,10 @@ gulp.task('serve', ['build'], function(done) {
     port: 9000,
     server: {
       baseDir: ['.'],
-      middleware: function(req, res, next) {
+      middleware: [proxy(apiProxyOptions),function(req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
-      }
+      }]
     }
   }, done);
 });
