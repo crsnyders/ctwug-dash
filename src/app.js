ff58f10736731bb9da35be07a17c86e5ef938d0e
Christopher Snyders <csnyders@thefieldoffice.co.za>
2017-03-24 06:44:37 +0200

    fixed name and fixed queue users width

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'CTWUG-DASH';
     config.map([
       { route: ['', 'transmission'], name: 'transmission-remote',      moduleId: 'transmission-remote',      nav: true, title: 'Transmission' },
       { route: 'dc', name: 'dc',      moduleId: 'eiskaltdcpp',      nav: true, title: 'DC++' },
